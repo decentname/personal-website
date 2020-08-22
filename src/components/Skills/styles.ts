5dfffb39406901761e40ac627e7e8d2312bf58e2
Himanshu Gupta <mr.hemangpta@gmail.com>
2020-08-22 17:15:19 +0530

    black colour matters

--- src/components/Skills/styles.ts
+++ src/components/Skills/styles.ts
@@ -14,5 +14,5 @@ ${tw`flex items-center justify-center w-full flex-wrap`};
 `;
 
 export const Skill = styled.span`
-  ${tw`flex items-center justify-center w-40 h-10 text-indigo-500 border border-teal-400 rounded-full mb-2 mx-2`};
+  ${tw`flex items-center justify-center w-40 h-10 border border-teal-400 rounded-full mb-2 mx-2`};
 `;
