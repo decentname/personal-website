398722df2697c86792c2638a4eb5096e7238f502
himanshu0128 <mr.hemangpta@gmail.com>
2021-06-19 18:55:45 +0530

    add dev.to link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,30 +3,30 @@ import React from 'react';
 import Container from 'components/ui/Container';
 import Icon from 'components/ui/Icon';
 
-
 import * as Styled from './styles';
 
 const Footer: React.FC = () => (
   <Styled.Footer>
     <Container>
       <Styled.Links>
-        <Styled.Link href="https://github.com/decentname" rel="noreferrer noopener" target="_blank">
+        <Styled.Link href="https://github.com/himanshu0128" rel="noreferrer noopener" target="_blank">
           <Styled.Icon>
             <Icon icon={['fab', 'github']} />
           </Styled.Icon>
         </Styled.Link>
-        <Styled.Link
-          href="https://twitter.com/himanshu0128"
-          rel="noreferrer noopener"
-          target="_blank"
-        >
+        <Styled.Link href="https://twitter.com/himanshu0128" rel="noreferrer noopener" target="_blank">
           <Styled.Icon>
             <Icon icon={['fab', 'twitter']} />
           </Styled.Icon>
         </Styled.Link>
         <Styled.Link href="https://linkedin.com/in/himanshu0128" rel="noreferrer noopener" target="_blank">
-        <Styled.Icon>
+          <Styled.Icon>
             <Icon icon={['fab', 'linkedin']} />
+          </Styled.Icon>
+        </Styled.Link>
+        <Styled.Link href="https://dev.to/himanshu0128" rel="noreferrer noopener" target="_blank">
+          <Styled.Icon>
+            <Icon icon={['fab', 'dev']} />
           </Styled.Icon>
         </Styled.Link>
       </Styled.Links>
